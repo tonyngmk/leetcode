1620f850edb83518dc29292cd3f8693bc7486ecc
Tony <tonyngmengkiat@gmail.com>
2022-06-27 10:18:53 +0800

    Completed contains duplicate

--- algorithms/binarySearch/sqrt_x.py
+++ algorithms/binarySearch/sqrt_x.py
@@ -2,15 +2,26 @@
 x = 8; ans = 2
 
 
+from math import e, log
+
 class Solution(object):
     def mySqrt(self, x):
         """
         :type x: int
         :rtype: int
         """
+        # # Approach 1: pocket calculator
+        # if x < 2: return x
+        # root = int(e**(0.5 * log(x)))
+        # root_plus = root + 1
+        # return root if root_plus**2 > x else root_plus
+    
+        # Approach 2 : binary search
+        
         i=2
         j=x/2
-        m=0
+        m=x
+        if x < 2: return x
         while i <= j:
             m = i + (j-i)//2
             if m**2 == x:
@@ -20,7 +31,8 @@ class Solution(object):
             else:
                 j = m - 1
         
-        return int(m)
+        return j
+
 
 solution = Solution()
 print(solution.mySqrt(x))
---
+++ sequence/README.md
@@ -0,0 +1,5 @@
+# Leetcode Challenge
+
+### Sean Prashad Leetcode Patterns
+- https://seanprashad.com/leetcode-patterns/
+- Structured in a way where similar questions are ordered together, from easy to difficult, according to difficulty of data structure.
