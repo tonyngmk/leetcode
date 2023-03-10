c0bb974396875eb935ccb09474dc5bc4d8e2be6d
Tony <tonyngmengkiat@gmail.com>
2023-03-10 23:42:22 +0800

    add contains duplicate

---
+++ python/217_contains_duplicate/217_sort_method_debug.py
@@ -0,0 +1,38 @@
+from typing import List
+
+def serialize(arr, i=0):
+    """Serialize an array into a format the visualizer can understand."""
+    formatted = {
+        "kind": {"grid": True},
+        "rows": [
+            {
+                "columns": [
+                    {"content": str(value), "tag": str(value)} for value in arr
+                ],
+            }
+        ],
+         "markers": [
+        {
+            "row": 0,
+            "column": i,
+            "id": "i",
+        },
+    ]
+    }
+    return formatted
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        serialized = serialize(nums, 0)
+        nums.sort()
+        serialized = serialize(nums, 0)
+        
+        for i in range(1, len(nums)):
+            serialized = serialize(nums, i)
+            if nums[i] == nums[i-1]: return True
+        return False
+
+
+# assert(Solution().containsDuplicate([1,2,3,1])==True)
+assert(Solution().containsDuplicate([1,2,3])==False)
--- python/217_contains_duplicate/README.md
+++ python/217_contains_duplicate/README.md
@@ -1,3 +1,7 @@
 ### 1. Set method:
 - Time: O(1)
 - Space: O(n)
+
+### 2. Sort method:
+- Time: O(n log n)
+- Space: O(1)
