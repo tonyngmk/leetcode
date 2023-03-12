a3f9b7ce923e2ee4ce54adca4eed6d792ac2f7ee
Tony <tonyngmengkiat@gmail.com>
2023-03-12 08:27:12 +0800

    test commit for adding github email

--- python/268_missing_number/268_xor_debug.py
+++ python/268_missing_number/268_xor_debug.py
@@ -39,6 +39,6 @@ class Solution:
             
         return ans
 
-# assert(Solution().missingNumber([3,0,1]) == 2)
-# assert(Solution().missingNumber([0,1]) == 2)
+assert(Solution().missingNumber([3,0,1]) == 2)
+assert(Solution().missingNumber([0,1]) == 2)
 assert(Solution().missingNumber([9,6,4,2,3,5,7,0,1]) == 8)
