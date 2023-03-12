31cd6f66a2671c4bf41554cbfd50d0c096735045
Tony <tonyngmengkiat@gmail.com>
2023-03-12 16:12:16 +0800

    add 21 merge two sorted lists

---
+++ python/21_merge_two_sorted_lists/21_sentinel.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Time: O(n)
+        # Space: O(1)
+
+        s = ListNode(None)
+        temp = s
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                s.next = l1
+                l1 = l1.next
+            else:
+                s.next = l2
+                l2 = l2.next
+            s = s.next
+            
+        s.next = l1 if l1 else l2
+        
+        return temp.next
