9940aa01038d05ed3ad526d51af4808ffec1b4a9
Tony <tonyngmengkiat@gmail.com>
2023-03-13 09:15:34 +0800

    add minor changes to 101 recursion

--- python/101_symmetric_trees/101_recursion.py
+++ python/101_symmetric_trees/101_recursion.py
@@ -8,13 +8,12 @@ class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
         
         def check(l, r):
-        
             if not l and not r: return True
             if not l or not r: return False
+            if l.val != r.val: return False
 
             return (
-                (l.val == r.val) and
-                check(l.left, r.right) and 
+                check(l.left, r.right) and
                 check(l.right, r.left)
             )
         
