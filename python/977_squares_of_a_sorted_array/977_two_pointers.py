b00b6b6dc87f3dbcbdb68f3b01640e6a8a6e71ef
Tony <tonyngmengkiat@gmail.com>
2023-03-14 19:31:07 +0800

    add 977 sqaures of a sorted array

---
+++ python/977_squares_of_a_sorted_array/977_two_pointers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        
+        ans = []
+        i, j = 0, len(nums)-1
+        
+        while i <= j:
+            a, b = nums[i]**2, nums[j]**2
+            if a > b:
+                ans.append(a)
+                i += 1
+            else:
+                ans.append(b)
+                j -= 1
+        
+        return ans[::-1]
+            
