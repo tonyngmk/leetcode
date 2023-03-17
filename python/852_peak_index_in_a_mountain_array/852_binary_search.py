2c03a80530113eea7460d94c677d158e0522abe4
Tony <tonyngmengkiat@gmail.com>
2023-03-17 15:12:14 +0800

    add 852 peak index in a mountain array

---
+++ python/852_peak_index_in_a_mountain_array/852_binary_search.py
@@ -0,0 +1,12 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        
+        i, j = 0, len(arr)-1
+        
+        while i < j:
+            mid = i + (j-i)//2
+            if arr[mid] < arr[mid+1]: i = mid + 1
+            else: j = mid
+                
+        return i
+        
