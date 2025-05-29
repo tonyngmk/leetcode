4810d8d367f890ae38b4a4a4116b66bdaed4023e
Tony Ng <tonyngmengkiat@gmail.com>
2025-05-29 15:17:57 +0800

    update

--- python/1_two_sum/1_two_sum.py
+++ python/1_two_sum/1_two_sum.py
@@ -1,4 +1,10 @@
+from typing import List
 class Solution:
+    def __init__(self):
+        self.nums = [2,7,11,15]
+        self.target = 9
+        print(self.twoSum(self.nums, self.target))
+
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         d = {j:i for i, j in enumerate(nums)}
@@ -6,3 +12,5 @@ class Solution:
         for i in range(len(nums)):
             comp = target - nums[i]
             if comp in d and d[comp] != i: return [i, d[comp]]
+
+sol = Solution()
